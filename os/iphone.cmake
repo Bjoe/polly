e543e565a9f9dbee32c03df60d441a3888436414
Joerg-Christian Boehme <joerg@chaosdorf.de>
2019-09-23 15:34:18 +0200

    Changes for iOS 13 SDK

--- os/iphone.cmake
+++ os/iphone.cmake
@@ -26,6 +26,12 @@ if(CMAKE_VERSION VERSION_LESS "3.5")
   )
 endif()
 
+if("${IOS_SDK_VERSION}" VERSION_GREATER_EQUAL "13.0" AND "${CMAKE_VERSION}" VERSION_LESS "3.14")
+  polly_fatal_error(
+      "CMake minimum required version for iOS SDK Version ${IOS_SDK_VERSION} is 3.14 (current ver: ${CMAKE_VERSION})"
+  )
+endif()
+
 string(COMPARE EQUAL "$ENV{DEVELOPER_DIR}" "" _is_empty)
 if(NOT _is_empty)
   polly_status_debug("Developer root (env): $ENV{DEVELOPER_DIR}")
@@ -148,11 +154,25 @@ set(CMAKE_XCODE_ATTRIBUTE_VALID_ARCHS[sdk=iphonesimulator*] "${archs}")
 # Introduced in iOS 9.0
 set(CMAKE_XCODE_ATTRIBUTE_ENABLE_BITCODE NO)
 
-# This will set CMAKE_CROSSCOMPILING to TRUE.
-# CMAKE_CROSSCOMPILING needed for try_run:
-# * https://cmake.org/cmake/help/latest/command/try_run.html#behavior-when-cross-compiling
-# (used in CURL)
-set(CMAKE_SYSTEM_NAME "Darwin")
+if("${CMAKE_VERSION}" VERSION_GREATER_EQUAL "3.14")
+  # set proper system name see https://cmake.org/cmake/help/v3.14/manual/cmake-toolchains.7.html#cross-compiling-for-ios-tvos-or-watchos
+  set(CMAKE_SYSTEM_NAME "iOS")
+  # set(CMAKE_OSX_DEPLOYMENT_TARGET "${CMAKE_XCODE_ATTRIBUTE_IPHONEOS_DEPLOYMENT_TARGET}")
+  # set(CMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH NO)
+  # set(CMAKE_IOS_INSTALL_COMBINED YES)
+  # foreach(arch ${IPHONEOS_ARCHS})
+  #   list(APPEND CMAKE_OSX_ARCHITECTURES "${arch}")
+  # endforeach()
+  # foreach(arch ${IPHONESIMULATOR_ARCHS})
+  #   list(APPEND CMAKE_OSX_ARCHITECTURES "${arch}")
+  # endforeach()
+else()
+  # This will set CMAKE_CROSSCOMPILING to TRUE.
+  # CMAKE_CROSSCOMPILING needed for try_run:
+  # * https://cmake.org/cmake/help/latest/command/try_run.html#behavior-when-cross-compiling
+  # (used in CURL)
+  set(CMAKE_SYSTEM_NAME "Darwin")
+endif()
 
 # Set CMAKE_SYSTEM_PROCESSOR for one-arch toolchain
 # (needed for OpenCV 3.3)
