19913003e654d38ecef04b6f3051272a6394eef6
Joerg-Christian Boehme <joerg@chaosdorf.de>
2020-02-04 08:15:41 +0100

    Add clang/gcc ninja C++17 toolchains

---
+++ clang-ninja-cxx17-libcxx.cmake
@@ -0,0 +1,18 @@
+if(DEFINED POLLY_CLANG_CXX17_CMAKE_)
+  return()
+else()
+  set(POLLY_CLANG_CXX17_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+polly_init(
+    "clang / LLVM Standard C++ Library (libc++) / c++17 support"
+    "Ninja"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/library/std/libcxx.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake")
---
+++ clang-ninja-cxx17.cmake
@@ -0,0 +1,17 @@
+if(DEFINED POLLY_CLANG_CXX17_CMAKE_)
+  return()
+else()
+  set(POLLY_CLANG_CXX17_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+polly_init(
+    "clang / c++17 support"
+    "Ninja"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake")
